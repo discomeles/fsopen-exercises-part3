d46cd584e28ef52a63b62f6678f44b51affe3856
PNiemi <pauliina.niemi@kapsi.fi>
2023-10-31 09:44:35 +0200

    3.7 ja 3.8 puhelinluettelon backend: step7 ja 8, loggaus morganilla

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+// lisätään morganin logattavaksi
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body, ["name","number"])
+  })
 
 let persons = [
   {
