abb4559969ec28c47cd43ebe81428008c622189b
PNiemi <pauliina.niemi@kapsi.fi>
2023-11-02 09:10:51 +0200

    3.17 puhelinluettelo ja tietokanta, step 5 tiedon muutos

--- index.js
+++ index.js
@@ -99,6 +99,23 @@ app.post('/api/persons', (req,res) => {
   })
 })
 
+// --- Yhteystiedon muuttaminen ---
+
+app.put('/api/persons/:id', (req,res,next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  
+  Person.findByIdAndUpdate(req.params.id, person, {new:true})
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
